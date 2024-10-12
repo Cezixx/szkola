0413a9970effd391524ca43822d8f552b746fab3
Cezixx <cmarkiewicz60@gmail.com>
2024-10-12 10:02:44 +0200

    First project

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-init
+public class Main {
+    public static void main(String[] args) {
+        int MyAge = 20;
+        int DrivingAge = 18;
+
+        if (MyAge >= DrivingAge) {
+            System.out.println("Yes, I can drive");
+        } else {
+            System.out.println("No, I can't drive");
+        }
+    }
+}
